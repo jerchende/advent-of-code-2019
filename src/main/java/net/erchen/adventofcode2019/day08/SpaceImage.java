8696c42095e495f3a98daa04e0bab0891ee20bb0
Johannes Erchen <johannes@erchen-it.de>
2021-12-30 08:59:33 +0100

    DAY-08 solve day08

---
+++ src/main/java/net/erchen/adventofcode2019/day08/SpaceImage.java
@@ -0,0 +1,64 @@
+package net.erchen.adventofcode2019.day08;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+@Getter
+public class SpaceImage {
+
+    private final int[] image;
+    private final int width;
+    private final int height;
+    private final int layerSize;
+    private final int layerCount;
+
+    public SpaceImage(String input, int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.image = input.trim().chars().map(c -> Character.getNumericValue((char) c)).toArray();
+        this.layerSize = width * height;
+        this.layerCount = this.image.length / this.layerSize;
+    }
+
+    public IntStream image() {
+        return Arrays.stream(image);
+    }
+
+    public Stream<int[]> layers() {
+        return IntStream.range(0, layerCount).mapToObj(i -> image().skip((long) i * layerSize).limit(layerSize).toArray());
+    }
+
+    public int checkSum() {
+        return layers().min(Comparator.comparingInt(i -> countPixel(i, 0))).map(i -> countPixel(i, 1) * countPixel(i, 2)).orElseThrow();
+    }
+
+    public int countPixel(int[] input, int countPixel) {
+        return (int) Arrays.stream(input).filter(i -> i == countPixel).count();
+    }
+
+    public String toString() {
+        var sb = new StringBuilder();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                layer:
+                for (int layer = 0; layer < layerCount; layer++) {
+                    switch (image[layer * layerSize + y * width + x]) {
+                    case 0:
+                        sb.append("⬛️");
+                        break layer;
+                    case 1:
+                        sb.append("⬜️");
+                        break layer;
+                    }
+                }
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
---
+++ src/test/java/net/erchen/adventofcode2019/day08/SpaceImageTest.java
@@ -0,0 +1,69 @@
+package net.erchen.adventofcode2019.day08;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SpaceImageTest {
+
+    @SneakyThrows
+    static String solutionInput() {
+        return Files.readString(Path.of("src/test/resources/day08/input.txt")).trim();
+    }
+
+    @Test
+    void shouldParse() {
+        SpaceImage image = new SpaceImage("123456789012", 3, 2);
+
+        assertThat(image.getImage()).hasSize(12);
+        assertThat(image.getWidth()).isEqualTo(3);
+        assertThat(image.getHeight()).isEqualTo(2);
+        assertThat(image.getLayerCount()).isEqualTo(2);
+    }
+
+    @Test
+    void shouldReturnLayers() {
+        SpaceImage image = new SpaceImage("123456789012", 3, 2);
+
+        assertThat(image.layers()).containsExactly(new int[] { 1, 2, 3, 4, 5, 6 }, new int[] { 7, 8, 9, 0, 1, 2 });
+    }
+
+    @Test
+    void shouldCalculateChecksum() {
+        SpaceImage image = new SpaceImage("112011021000", 3, 2);
+
+        assertThat(image.checkSum()).isEqualTo(4);
+    }
+
+    @Test
+    void shouldCalculateChecksum_Solution() {
+        SpaceImage image = new SpaceImage(solutionInput(), 6, 25);
+
+        assertThat(image.checkSum()).isEqualTo(2016);
+    }
+
+    @Test
+    void shouldPrintImage() {
+        SpaceImage image = new SpaceImage("0222112222120000", 2, 2);
+
+        assertThat(image.toString()).isEqualTo("⬛️⬜️\n⬜️⬛️\n");
+    }
+
+    @Test
+    void shouldPrintImage_Solution() {
+        SpaceImage image = new SpaceImage(solutionInput(), 25, 6);
+
+        assertThat(image.toString()).isEqualTo("""
+                ⬜️⬛️⬛️⬜️⬛️⬜️⬜️⬜️⬜️⬛️⬛️⬜️⬜️⬛️⬛️⬜️⬜️⬜️⬜️⬛️⬜️⬛️⬛️⬜️⬛️
+                ⬜️⬛️⬛️⬜️⬛️⬛️⬛️⬛️⬜️⬛️⬜️⬛️⬛️⬜️⬛️⬛️⬛️⬛️⬜️⬛️⬜️⬛️⬛️⬜️⬛️
+                ⬜️⬜️⬜️⬜️⬛️⬛️⬛️⬜️⬛️⬛️⬜️⬛️⬛️⬛️⬛️⬛️⬛️⬜️⬛️⬛️⬜️⬛️⬛️⬜️⬛️
+                ⬜️⬛️⬛️⬜️⬛️⬛️⬜️⬛️⬛️⬛️⬜️⬛️⬛️⬛️⬛️⬛️⬜️⬛️⬛️⬛️⬜️⬛️⬛️⬜️⬛️
+                ⬜️⬛️⬛️⬜️⬛️⬜️⬛️⬛️⬛️⬛️⬜️⬛️⬛️⬜️⬛️⬜️⬛️⬛️⬛️⬛️⬜️⬛️⬛️⬜️⬛️
+                ⬜️⬛️⬛️⬜️⬛️⬜️⬜️⬜️⬜️⬛️⬛️⬜️⬜️⬛️⬛️⬜️⬜️⬜️⬜️⬛️⬛️⬜️⬜️⬛️⬛️
+                """);
+    }
+}
